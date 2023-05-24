df6d826204b6da94a32c0ce75d24a214123c064f
toan.tm <toan.tm@evotek.vn>
2023-05-24 09:01:56 +0700

    update enviroment

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from "react-i18next";
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ src/i18n/i18n.ts
@@ -0,0 +1,35 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import English from "../i18n/en.json";
+import Vietnamese from "../i18n/vi.json";
+import Backend from "i18next-http-backend";
+
+let getLocalLang = localStorage.getItem("lang");
+
+if (!getLocalLang) {
+  getLocalLang = "vie";
+  console.log("????");
+}
+const resources = {
+  eng: {
+    translation: English,
+  },
+  vie: {
+    translation: Vietnamese,
+  },
+};
+
+i18n
+  .use(Backend)
+
+  .use(initReactI18next)
+  .init({
+    resources,
+    debug: true,
+    lng: getLocalLang,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
